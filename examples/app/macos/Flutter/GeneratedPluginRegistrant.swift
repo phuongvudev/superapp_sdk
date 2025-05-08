98d7cf941f7d9508d98a794dc27213265caff860
alvin <phuong.vu.dev@gmail.com>
2025-05-08 14:29:14 +0700

    Initialize Flutter project structure with essential files and configurations for example

---
+++ examples/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import app_links
+import core
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  CorePlugin.register(with: registry.registrar(forPlugin: "CorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
